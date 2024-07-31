87616e30be324ef61f0bda1ade9c35b291b6509d
Ezequiel Parziale <eparziale@gmail.com>
2024-07-31 19:03:21 -0300

    :card_file_box: feat: add turso db conn

--- src/lib/prismadb.ts
+++ src/lib/prismadb.ts
@@ -1,5 +1,15 @@
-import { PrismaClient } from "@prisma/client"
 import "server-only"
+
+import { createClient } from "@libsql/client"
+import { PrismaLibSQL } from "@prisma/adapter-libsql"
+import { PrismaClient } from "@prisma/client"
+
+const libsql = createClient({
+  url: `${process.env.TURSO_DATABASE_URL}`,
+  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+})
+
+const adapter = new PrismaLibSQL(libsql)
 
 declare global {
   var prisma: PrismaClient | undefined
@@ -8,6 +18,7 @@ declare global {
 const prismadb =
   globalThis.prisma ||
   new PrismaClient({
+    adapter,
     log:
       process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   })
